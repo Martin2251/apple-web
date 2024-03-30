8b54dad5c2397bb8942321a45cd9796e97d6815f
Martin Lowe <martinlowe673@gmail.com>
2024-03-30 16:11:41 +0000

    added classes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { appleImg, bagImg, searchImg } from "../utils"
 
 const Navbar = () => {
   return (
- <header>
-    <nav>
+ <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+    <nav className="flex w-full screen-max-width">
         <img src={appleImg} alt="apple" width={14} height={18}/>
         <div>
             {['phones', 'Macbooks','Tablets'].map((nav,i) =>(
@@ -15,7 +15,7 @@ const Navbar = () => {
         </div>
         <div>
             <img src={searchImg} alt="search" width={18} height={18} />
-            <img src={bagImg} alt="search" width={18} height={18} />
+            <img src={bagImg} alt="bag" width={18} height={18} />
         </div>
     </nav>
  </header>
