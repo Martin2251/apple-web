b7197a587314fe40fed183369ea8f0794cefbfbb
Martin Lowe <martinlowe673@gmail.com>
2024-03-30 16:09:17 +0000

    search and bag

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { appleImg } from "../utils"
+import { appleImg, bagImg, searchImg } from "../utils"
 
 
 const Navbar = () => {
@@ -7,7 +7,15 @@ const Navbar = () => {
     <nav>
         <img src={appleImg} alt="apple" width={14} height={18}/>
         <div>
-            {['phones', 'Macbooks','Tablets']}
+            {['phones', 'Macbooks','Tablets'].map((nav,i) =>(
+                <div key={i}>
+                    {nav}
+                </div>
+            ))}
+        </div>
+        <div>
+            <img src={searchImg} alt="search" width={18} height={18} />
+            <img src={bagImg} alt="search" width={18} height={18} />
         </div>
     </nav>
  </header>
