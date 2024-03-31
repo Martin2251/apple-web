2043ea44cbd8edf842b9cb013b5bec9055980f4e
Martin Lowe <martinlowe673@gmail.com>
2024-03-31 20:53:37 +0100

    handle video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,26 @@ import gsap from "gsap"
 import { heroVideo,smallHeroVideo } from "../utils"
 import { useState } from "react"
 import { useGSAP } from "@gsap/react"
+import { useEffect } from "react"
 
 
 const Hero = () => {
   const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo :heroVideo)
   
+  const handleVideoSrcSet = () =>{
+    if(window.innerWidth < 760){
+      setVideoSrc(smallHeroVideo)
+    }else{
+      setVideoSrc(heroVideo)
+    }
+  }
+
+  useEffect(() =>{
+    window.addEventListener("resize",handleVideoSrcSet)
+    return()=>{
+      window.removeEventListener("resize",handleVideoSrcSet)
+    }
+  })
 
   useGSAP(() => {
     gsap.to('#hero', {opacity:1, delay:1.5})
