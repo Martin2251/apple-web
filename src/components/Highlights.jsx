db18780a8faec94e877bf5d15b62143c333b68ee
Martin Lowe <martinlowe673@gmail.com>
2024-03-31 21:04:21 +0100

    highlights

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -2,9 +2,14 @@
 
 const Highlights = () => {
   return (
+<section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
+  <div className="screen-max-width">
     <div>
-      
+      <h1 className="section-heading">get the highlights</h1>
     </div>
+  </div>
+
+</section>
   )
 }
 
