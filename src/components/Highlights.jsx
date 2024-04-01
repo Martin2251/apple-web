f5a4aa96a3126dc142620e36e602b1f3f2496951
Martin Lowe <martinlowe673@gmail.com>
2024-04-01 13:05:40 +0100

    highlights

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,11 +1,12 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
-import { watchImg } from "../utils"
+import { watchImg,rightImg } from "../utils"
 
 const Highlights = () => {
 
   useGSAP(() =>{
     gsap.to('#title', {opacity:1,y:0})
+    gsap.to('.link',{opacity:1, y:0})
   })
   return (
 <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
@@ -13,8 +14,12 @@ const Highlights = () => {
     <div className="mb-12 w-full items-end justify-between">
       <h1 id="title" className="section-heading">get the highlights</h1>
       <div className="flex flex-wrap items-end gap-5">
-        <p className="link"> watch the film</p>
+        <p className="link"> watch the film
         <img src={watchImg} alt="watch" className="m-l-2"  />
+        </p>
+        <p className="link"> watch the event
+        <img src={rightImg} alt="right" className="m-l-2"  />
+        </p>
       </div>
     </div>
   </div>
